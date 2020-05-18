ec7ea4f0342d4bb37e050d8821fbc99f36439954
sparshsamir <sparshsamir1993@gmail.com>
2020-05-18 00:55:25 -0400

    Second commit. map not showing

---
+++ app/src/main/java/com/example/memorableplaces/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.memorableplaces;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void addPlaceClick(View view){
+        Intent toMapScreen = new Intent(getApplicationContext(), MapsActivity.class);
+        startActivity(toMapScreen);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
